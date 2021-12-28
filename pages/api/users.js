db9e80f35da05e1f80d323a6221ef0f90f4b4543
alirezamoon <khanamani1998@gmail.com>
2021-12-28 11:43:36 +0330

    fixed post request

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,15 +1,15 @@
-import clientPromise from '../../lib/mongodb'
+import clientPromise from "../../lib/mongodb"
 
 export default async (req, res) => {
-  console.log('req', req)
-  if (req.method === 'GET') {
-    const client = await clientPromise
-    const db = client.db('train')
-    let users = await db.collection('passengers').find({}).toArray()
+  console.log("req", req)
+  const client = await clientPromise
+  const db = client.db("train")
+  if (req.method === "GET") {
+    let users = await db.collection("passengers").find({}).toArray()
     users = JSON.parse(JSON.stringify(users))
     res.status(200).json(users)
-  } else if (req.method === 'POST') {
-    db.collection('passengers').insertOne(req.body, function (err, res) {
+  } else if (req.method === "POST") {
+    db.collection("passengers").insertOne(req.body, function (err, res) {
       if (err) console.log(err)
     })
     res.status(201).json()
